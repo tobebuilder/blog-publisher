fa6c0fa1495175d1370f2d0991008ebf2779df6f
tony <262013818@qq.com>
2026-05-13 21:26:11 +0800

    fix: correct Pool import from pg-cloudflare

--- blog/src/lib/prisma.ts
+++ blog/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '../generated/prisma/edge'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg-cloudflare'
+import Pool from 'pg-cloudflare'
 
 const connectionString = process.env.DATABASE_URL
 
