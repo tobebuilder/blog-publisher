cc07687b8977fee56214ff412f90c808fc03bc7f
tony <262013818@qq.com>
2026-05-13 16:18:09 +0800

    perf: deep optimization for cloudflare worker size limits

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client', 'pg', 'pg-cloudflare'],
+  experimental: {
+    serverMinification: true,
+  },
 };
 
 export default nextConfig;
--- blog/src/lib/prisma.ts
+++ blog/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma'
+import { PrismaClient } from '../generated/prisma/edge'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
