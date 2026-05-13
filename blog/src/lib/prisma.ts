6f763c70e5f45174632bca2886ea2e25196ab502
tony <262013818@qq.com>
2026-05-13 21:30:46 +0800

    fix: bypass typescript type check for pg-cloudflare Pool

--- blog/src/lib/prisma.ts
+++ blog/src/lib/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from '../generated/prisma/edge'
 import { PrismaPg } from '@prisma/adapter-pg'
-import Pool from 'pg-cloudflare'
+import pg from 'pg-cloudflare'
 
 const connectionString = process.env.DATABASE_URL
 
-const pool = new Pool({ connectionString })
+const pool = new (pg as any).Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
